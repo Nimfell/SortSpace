b24be27591d38594d7e7de74d991b4db31327edf
Nimfell <44020287+Nimfell@users.noreply.github.com>
2019-10-26 11:39:03 +0300

    Add files via upload

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -2,40 +2,41 @@
 using System.Collections.Generic;
 
 namespace SortSpace
-{      
+{
     public class BinarySearch
     {
-        public  int[] SortedArray;
-        public  int   Right, Left;
-        private int   Result; 
-       
+        int count;
+        public int[] SortedArray;
+        public int Right, Left;
+        private int Result;
+
         public BinarySearch(int[] S_Array)
-        {            
+        {
             Left = 0;
             Right = S_Array.Length - 1;
             SortedArray = S_Array;
         }
 
-        public void Step(int N) 
+        public void Step(int N)
         {
-
             if (Result == 1 || Result == -1) return;
-            int middle = (Left + ((Right - Left + 1 ) / 2) ); //делит текущий диапазон на два
-            if (N == SortedArray[middle]) 
+            int middle = (Right - ((Right - Left + 1) / 2)); //делит текущий диапазон на два
+            //Console.WriteLine("  Middle = " + middle);
+            //Console.WriteLine("  Items  = " + (Right - Left + 1) );
+            if (N == SortedArray[middle])
             {   Result = 1;
                 return;
             }
             if (Right == Left) // значение не найдено
-            {
-                Result = -1;
+            {   Result = -1;
                 return;
             }
-
-            if (N < SortedArray[middle]) 
-                Right = middle - 1;   //продолжает сокращение рабочего диапазона, корректируя Left и Right, 
-            else                         
+            if (N < SortedArray[middle])
+            {   if (middle == 0) Right = 0;
+                else Right = middle - 1;   //продолжает сокращение рабочего диапазона, корректируя Left и Right,
+            }
+            else
                 Left = middle + 1;
-
             Result = 0;
         }
 
@@ -43,42 +44,51 @@ namespace SortSpace
 
         public int Search(int N)
         {
+            count = 0;
             int result = 0;
             while (result == 0)
-            {   Step(N);
-                result = GetResult();
+            {
+                count++;
+                //Console.WriteLine("  Step: " + count);
+                //Console.WriteLine("  Left   = " + Left);
+                //Console.WriteLine("  Right  = " + Right);
+                //Console.WriteLine();
+                Step(N);
+                result = GetResult();                
             }
             return result;
-        } 
-   
+        }
+
         public void Reset()
         {
-            Result = 0; 
-            Left = 0; 
+            Result = 0;
+            Left = 0;
             Right = SortedArray.Length - 1;
         }
     }
+    
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {   
+    //        int[] arr_1 = new int[99];
 
-    /*
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr_1 = new int[] { -4, -3, -2, -1, 0, 1, 2, 4 };
-            BinarySearch Arr = new BinarySearch(arr_1);            
-            Console.Write(Arr.Search(-8));
-            Arr.Reset();
-            Console.Write(Arr.Search(8));
-            Arr.Reset();
-            Console.Write(Arr.Search(-4));
-            Arr.Reset();
-            Console.Write(Arr.Search(4));
-            Arr.Reset();
-            Console.Write(Arr.Search(-2));
-            Arr.Reset();            
-            Console.Write(Arr.Search(2));            
-            Console.WriteLine();
-        }        
-    }
-	*/
+    //        for (int i = 0; i < arr_1.Length; i++ )
+    //        {
+    //            arr_1[i] = i + 1;
+    //        }
+    //        BinarySearch Arr = new BinarySearch(arr_1);            
+    //        //Console.Write(Arr.Search(49));
+    //        //Arr.Reset();
+    //        Console.WriteLine(Arr.Search(100));
+    //        Arr.Reset();
+    //        Console.WriteLine(Arr.Search(-1));
+    //        Arr.Reset();
+    //        Console.WriteLine(Arr.Search(99));
+    //        Arr.Reset();
+    //        Console.WriteLine(Arr.Search(1));
+    //        Arr.Reset();
+    //        Console.WriteLine(Arr.Search(2));
+    //    }        
+    //}	
 }
